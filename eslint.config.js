7dab011ffe96905187e022bda5849845126a215d
zhen237 <gao15135029905@163.com>
2025-06-27 11:13:43 +0800

    团队代码规范

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,29 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
 import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginVue, { rules } from 'eslint-plugin-vue'
+import skipFormatting, { ignores } from '@vue/eslint-config-prettier/skip-formatting'
 
 export default defineConfig([
   {
     name: 'app/files-to-lint',
     files: ['**/*.{js,mjs,jsx,vue}'],
+    rules: {
+      'vue/muti-words-component-names': [
+        'warn',
+        {
+          ignores: ['index'],
+        },
+      ],
+      'prettier/prettier': {
+        semi: false,
+        singleQuote: true,
+        printWidth: 100,
+        tabWidth: 2,
+        trailingComma: 'all',
+        endOfLine: 'auto',
+      },
+    },
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
---
+++ src/Login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>登录页</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
